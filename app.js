83f00b740fe1ea5ba3a414776503cb5f38231075
VinayKumar Prajapati <sagaipoint@gmail.com>
2024-07-03 16:58:31 +0530

    console added

--- app.js
+++ app.js
@@ -225,6 +225,8 @@ function convertToStringArray(commaString) {
 }
 app.post("/create-persona", async (req, res) => {
   try {
+    console.log("req body ===>", req.body);
+    return;
     const { jobSelect } = req.body;
     const selectedIds = Array.isArray(jobSelect) ? jobSelect : [jobSelect];
     const reqUUID = req.body.reqId || uuidv4();
