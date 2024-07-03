cdaa7129e43d513a776e1c18440fbad0e9f1fd40
VinayKumar Prajapati <sagaipoint@gmail.com>
2024-07-03 21:26:03 +0530

    commented

--- app.js
+++ app.js
@@ -187,9 +187,9 @@ app.post("/send-email", async (req, res) => {
       const jobLocation = foundJob.map((job) => job.job_location).join(", ");
       console.log("job post", jobPost);
       const mailOptions = {
-        to: email.email,
-        // to: "vinay.prajapati@hirequotient.com",
-        bcc: "sidhartha@hirequotient.com",
+        // to: email.email,
+        to: "vinay.prajapati@hirequotient.com",
+        bcc: "sidhartha@hirequotient.com,vinay.prajapati@hirequotient.com,amarthya@hirequotient.com",
         from: req.body.fromEmail,
         subject: subject
           .replaceAll("{name}", personData?.name)
