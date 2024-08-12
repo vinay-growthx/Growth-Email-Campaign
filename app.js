5804ff1f885e82acef806ae009f528f2445187a7
VinayKumar Prajapati <sagaipoint@gmail.com>
2024-08-12 22:17:42 +0530

    add email

--- app.js
+++ app.js
@@ -257,7 +257,7 @@ app.post("/send-email", async (req, res) => {
           const jobLocation = foundJob
             .map((job) => job.job_location)
             .join(", ");
-          jobPost = removeAfterFirstComma(jobPost) || "Open roles";
+          jobPost = removeAfterFirstComma(jobPost) || "Open Roles";
 
           let replacedSubject = subject
             .replaceAll("{name}", personData?.name)
